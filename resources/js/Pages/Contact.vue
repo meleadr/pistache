d52a9836220a36d09b1750b9160595ebf26e244d
meleadr <meledina.adriano@gmail.com>
2023-06-13 19:59:24 +0200

    Modification affichage contact

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -15,22 +15,33 @@ const contactInformation = {
     <Head title="Contactez-nous" />
 
     <WebsiteLayout>
+        <template #header>
+            <h2 class="text-4xl text-center mb-10">Nous Contacter</h2>
+        </template>
         <!-- Contact Information Section -->
         <section class="contact-info py-20">
-            <h2 class="text-4xl text-center mb-10">Nous Contacter</h2>
-            <div class="flex flex-col items-center space-y-6 text-center">
-                <p class="text-xl">{{ contactInformation.phone }}</p>
-                <p class="text-xl">{{ contactInformation.email }}</p>
-                <p class="text-xl">{{ contactInformation.address }}</p>
-                <p class="text-xl">{{ contactInformation.openingHours }}</p>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 text-center">
+                <div>
+                    <p class="text-xl">Adresse</p>
+                    <p class="text-xl">{{ contactInformation.address }}</p>
+                    <p class="text-xl">Horaires</p>
+                    <p class="text-xl">{{ contactInformation.openingHours }}</p>
+                </div>
+                <div>
+                    <p class="text-xl">Téléphone</p>
+                    <p class="text-xl">{{ contactInformation.phone }}</p>
+                    <p class="text-xl">Email</p>
+                    <p class="text-xl">{{ contactInformation.email }}</p>
+                </div>
             </div>
         </section>
 
         <!-- Google Map Section -->
-        <section class="map">
-            <div class="aspect-w-16 aspect-h-9">
+        <section class="map w-full">
+            <div class="aspect-w-16 aspect-h-9 w-full">
                 <iframe
-                    src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.761666666667!2d2.336761315674781!3d48.86272597928894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x4b9b0c1b0c0b0b0!2sPistache!5e0!3m2!1sfr!2sfr!4v1623349129129!5m2!1sfr!2sfr"
+                    class="w-full h-full"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.761666666667!2d2.336761315674781!3d48.86272597928894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x4b9b0c1b0c0b0b0!2sPistache!5e0!3m2!1sfr!2sfr!4v1623349129129!5m2!1sfr!2sfr"
                     style="border: 0"
                     allowfullscreen=""
                     loading="lazy"
@@ -39,5 +50,7 @@ const contactInformation = {
         </section>
     </WebsiteLayout>
 </template>
+
+<style scoped></style>
 
 <style scoped></style>
