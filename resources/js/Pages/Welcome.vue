675b7a3a32431b4c95287ade004bf414f3275985
meleadr <meledina.adriano@gmail.com>
2023-06-13 19:08:57 +0200

    Premiere structure

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,60 +1,64 @@
 <script setup>
+import { ref } from "vue";
 import { Head } from "@inertiajs/vue3";
-import GuestLayout from "@/Layouts/GuestLayout.vue";
+import WebsiteLayout from "@/Layouts/WebsiteLayout.vue";
 
-// defineProps({
-//     canLogin: {
-//         type: Boolean,
-//     },
-//     canRegister: {
-//         type: Boolean,
-//     },
-//     laravelVersion: {
-//         type: String,
-//         required: true,
-//     },
-//     phpVersion: {
-//         type: String,
-//         required: true,
-//     },
-// });
+// Hero data
+const heroImage = ref("/img/hero.jpg");
+const heroTitle = ref("Bienvenue sur notre site");
+
+// About data
+const aboutText = ref("Ceci est un texte à propos de nous.");
+
+// Inspiration data
+const inspirationImages = ref([
+    "/path/to/image1.jpg",
+    "/path/to/image2.jpg",
+    "/path/to/image3.jpg",
+]);
 </script>
 
 <template>
     <Head title="Welcome" />
 
-    <GuestLayout>
-        <!-- <div
-            class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+    <WebsiteLayout>
+        <!-- Section Banner / Hero -->
+        <section
+            class="hero bg-center bg-cover h-screen flex items-center justify-center"
+            :style="{ 'background-image': `url(${heroImage})` }"
         >
-            <div
-                v-if="canLogin"
-                class="sm:fixed sm:top-0 sm:right-0 p-6 text-right"
-            >
-                <Link
-                    v-if="$page.props.auth.user"
-                    :href="route('dashboard')"
-                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Dashboard</Link
-                >
-
-                <template v-else>
-                    <Link
-                        :href="route('login')"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                        >Log in</Link
-                    >
-
-                    <Link
-                        v-if="canRegister"
-                        :href="route('register')"
-                        class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                        >Register</Link
-                    >
-                </template>
+            <div class="hero-content text-white text-center">
+                <h1 class="text-5xl">{{ heroTitle }}</h1>
             </div>
-        </div> -->
-    </GuestLayout>
+        </section>
+
+        <!-- Section À propos -->
+        <section class="about py-20">
+            <h2 class="text-4xl text-center mb-10">Notre histoire</h2>
+            <p class="text-center">{{ aboutText }}</p>
+        </section>
+
+        <!-- Section Photos d'inspirations -->
+        <section class="inspiration py-20 grid grid-cols-3 gap-4">
+            <div v-for="(image, index) in inspirationImages" :key="index">
+                <img
+                    :src="image"
+                    :alt="`Inspiration image ${index + 1}`"
+                    class="w-full"
+                />
+            </div>
+        </section>
+
+        <!-- Section Call To Action -->
+        <section class="cta py-20 text-center">
+            <router-link
+                to="/contact"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Contactez-nous
+            </router-link>
+        </section>
+    </WebsiteLayout>
 </template>
 
-<style></style>
+<style scoped></style>
