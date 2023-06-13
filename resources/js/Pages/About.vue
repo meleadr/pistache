57afc0261539ba755a887b9686bd345b46305451
meleadr <meledina.adriano@gmail.com>
2023-06-13 20:06:22 +0200

    Ajout de la page "A propos"

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import WebsiteLayout from "@/Layouts/WebsiteLayout.vue";
+
+// About data
+const aboutText =
+    "Pistache est un restaurant unique qui mêle les saveurs de la France et de la Méditerranée. Notre mission est de vous faire vivre une expérience culinaire inoubliable, tout en partageant notre passion pour l'art de la gastronomie. Que vous cherchiez un endroit pour un dîner romantique, une réunion d'affaires, ou un repas en famille, Pistache est l'endroit idéal.";
+
+// Gallery data
+const galleryImages = [
+    "/img/repas1.webp",
+    "/img/repas2.jpg",
+    "/img/repas3.jpg",
+    "/img/repas4.jpg",
+];
+
+// Team data
+const teamMembers = [
+    { name: "Jean Dupont", role: "Chef", image: "/img/chef.jpg" },
+    { name: "Marie Leblanc", role: "Sommelière", image: "/img/sommelier.jpg" },
+    // Add more team members as needed
+];
+
+// Building data
+const buildingImage = "/img/building.jpg";
+const buildingText =
+    "Notre restaurant est situé dans un bel édifice historique en plein cœur de Paris. L'intérieur est décoré dans un style qui allie le charme traditionnel français et une touche de modernité.";
+</script>
+
+<template>
+    <Head title="À propos de nous" />
+
+    <WebsiteLayout>
+        <!-- About Section -->
+        <section class="about py-20">
+            <h2 class="text-4xl text-center mb-10">À propos de nous</h2>
+            <p class="text-center">{{ aboutText }}</p>
+        </section>
+
+        <!-- Gallery Section -->
+        <section class="gallery py-20 grid grid-cols-2 sm:grid-cols-4 gap-4">
+            <div v-for="(image, index) in galleryImages" :key="index">
+                <img
+                    :src="image"
+                    :alt="`Gallery image ${index + 1}`"
+                    class="w-full"
+                />
+            </div>
+        </section>
+
+        <!-- Team Section -->
+        <section class="team py-20">
+            <h2 class="text-4xl text-center mb-10">Notre équipe</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 text-center">
+                <div v-for="member in teamMembers" :key="member.name">
+                    <img
+                        :src="member.image"
+                        :alt="`Image of ${member.name}`"
+                        class="w-full"
+                    />
+                    <h3 class="text-xl">{{ member.name }}</h3>
+                    <p>{{ member.role }}</p>
+                </div>
+            </div>
+        </section>
+
+        <!-- Building Section -->
+        <section class="building py-20">
+            <h2 class="text-4xl text-center mb-10">Notre édifice</h2>
+            <div
+                class="flex flex-col sm:flex-row items-center space-y-6 sm:space-y-0 sm:space-x-6 text-center"
+            >
+                <img
+                    :src="buildingImage"
+                    alt="Image of our building"
+                    class="w-full sm:w-1/2"
+                />
+                <p class="sm:w-1/2">{{ buildingText }}</p>
+            </div>
+        </section>
+    </WebsiteLayout>
+</template>
+
+<style scoped></style>
