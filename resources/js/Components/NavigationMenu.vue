9867e8d43dc4dcbf2bfd126d400345f77a85fef8
meleadr <meledina.adriano@gmail.com>
2023-06-17 17:37:09 +0200

    Menus avec S

--- resources/js/Components/NavigationMenu.vue
+++ resources/js/Components/NavigationMenu.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="bg-white border-b border-gray-100">
+    <nav class="border-b border-gray-100 bg-white">
         <!-- Primary Navigation Menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex justify-between h-16">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 justify-between">
                 <div class="flex">
                     <!-- Logo -->
-                    <div class="shrink-0 flex items-center">
+                    <div class="flex shrink-0 items-center">
                         <Link href="/">
                             <ApplicationLogo
                                 class="block h-10 w-auto fill-current text-gray-800"
@@ -18,7 +18,7 @@
                         <NavLink
                             :href="route('menu')"
                             :active="route().current('menu')"
-                            >Menu</NavLink
+                            >Menus</NavLink
                         >
                         <NavLink
                             :href="route('about')"
