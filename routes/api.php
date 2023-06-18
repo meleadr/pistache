2356f429e8d93d972ab219595d3387956bd843d1
meleadr <meledina.adriano@gmail.com>
2023-06-18 12:37:59 +0200

    Route pour le CRUD

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\MenuCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// route for menu CRUD operations
+Route::controller(MenuController::class)->group(function () {
+	Route::get('/menus', 'getAllMenus');
+	Route::get('/menus/{id}', 'getMenuById');
+	Route::post('/menus', 'addMenu');
+	Route::put('/menus/{id}', 'updateMenu');
+	Route::delete('/menus/{id}', 'deleteMenu');
+});
+
+// route for category CRUD operations
+Route::controller(CategoryController::class)->group(function () {
+	Route::get('/categories', 'getAllCategories');
+	Route::get('/categories/{id}', 'getCategoryById');
+	Route::post('/categories', 'addCategory');
+	Route::put('/categories/{id}', 'updateCategory');
+	Route::delete('/categories/{id}', 'deleteCategory');
+});
+
+// route for menuHasCategory CRUD operations
+Route::controller(MenuHasCategoryController::class)->group(function () {
+	Route::get('/menuHasCategory', 'getAllMenuHasCategories');
+	Route::get('/menuHasCategory/{id}', 'getMenuHasCategoryById');
+	Route::post('/menuHasCategory', 'addMenuHasCategory');
+	Route::put('/menuHasCategory/{id}', 'updateMenuHasCategory');
+	Route::delete('/menuHasCategory/{id}', 'deleteMenuHasCategory');
 });
