c9f5b930db0f8e5bf5307483f90ccd6157acbc22
meleadr <meledina.adriano@gmail.com>
2023-06-18 13:12:53 +0200

    CRUD Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    //getAllCategories
+	public function getAllCategories()
+	{
+		$categories = Category::all();
+		return response()->json($categories);
+	}
+
+	//getCategoryById
+	public function getCategoryById($id)
+	{
+		$category = Category::find($id);
+		if(is_null($category)){
+			return response()->json(['message' => 'Category Not Found'], 404);
+		}
+		return response()->json($category);
+	}
+
+	//addCategory
+	public function addCategory(Request $request)
+	{
+		$category = Category::create($request->all());
+		return response($category, 201);
+	}
+
+	//updateCategory
+	public function updateCategory(Request $request, $id)
+	{
+		$category = Category::find($id);
+		if(is_null($category)){
+			return response()->json(['message' => 'Category Not Found'], 404);
+		}
+		$category->update($request->all());
+		return response($category, 200);
+	}
+
+	//deleteCategory
+	public function deleteCategory($id)
+	{
+		$category = Category::find($id);
+		if(is_null($category)){
+			return response()->json(['message' => 'Category Not Found'], 404);
+		}
+		$category->delete();
+		return response()->json(null, 204);
+	}
 }
