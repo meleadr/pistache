d8d93df63182378e3b1a2d28e20dfef5be65d956
meleadr <meledina.adriano@gmail.com>
2023-06-18 13:12:58 +0200

    CRUD Menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Menu;
 
 class MenuController extends Controller
 {
-    //
+    // getAllMenus
+	public function getAllMenus()
+	{
+		$menus = Menu::all();
+		return response()->json($menus);
+	}
+
+	// getMenuById
+	public function getMenuById($id)
+	{
+		$menu = Menu::find($id);
+		if(is_null($menu)){
+			return response()->json(['message' => 'Menu Not Found'], 404);
+		}
+		return response()->json($menu);
+	}
+
+	// addMenu
+	public function addMenu(Request $request)
+	{
+		$menu = Menu::create($request->all());
+		return response($menu, 201);
+	}
+
+	// updateMenu
+	public function updateMenu(Request $request, $id)
+	{
+		$menu = Menu::find($id);
+		if(is_null($menu)){
+			return response()->json(['message' => 'Menu Not Found'], 404);
+		}
+		$menu->update($request->all());
+		return response($menu, 200);
+	}
+
+	// deleteMenu
+	public function deleteMenu($id)
+	{
+		$menu = Menu::find($id);
+		if(is_null($menu)){
+			return response()->json(['message' => 'Menu Not Found'], 404);
+		}
+		$menu->delete();
+		return response()->json(null, 204);
+	}
 }
