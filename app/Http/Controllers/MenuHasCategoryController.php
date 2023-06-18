eafb07d4af6ac5a28406d9a1ea901085f1783f3a
meleadr <meledina.adriano@gmail.com>
2023-06-18 13:13:08 +0200

    CRUD MenuHasCategory

--- app/Http/Controllers/MenuHasCategoryController.php
+++ app/Http/Controllers/MenuHasCategoryController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MenuHasCategory;
 
 class MenuHasCategoryController extends Controller
 {
-    //
+    // getAllMenuHasCategories
+	public function getAllMenuHasCategories()
+	{
+		$menuHasCategories = MenuHasCategory::all();
+		return response()->json($menuHasCategories);
+	}
+
+	// getMenuHasCategoryById
+	public function getMenuHasCategoryById($id)
+	{
+		$menuHasCategory = MenuHasCategory::find($id);
+		if(is_null($menuHasCategory)){
+			return response()->json(['message' => 'MenuHasCategory Not Found'], 404);
+		}
+		return response()->json($menuHasCategory);
+	}
+
+	// addMenuHasCategory
+	public function addMenuHasCategory(Request $request)
+	{
+		$menuHasCategory = MenuHasCategory::create($request->all());
+		return response($menuHasCategory, 201);
+	}
+
+	// updateMenuHasCategory
+	public function updateMenuHasCategory(Request $request, $id)
+	{
+		$menuHasCategory = MenuHasCategory::find($id);
+		if(is_null($menuHasCategory)){
+			return response()->json(['message' => 'MenuHasCategory Not Found'], 404);
+		}
+		$menuHasCategory->update($request->all());
+		return response($menuHasCategory, 200);
+	}
+
+	// deleteMenuHasCategory
+	public function deleteMenuHasCategory($id)
+	{
+		$menuHasCategory = MenuHasCategory::find($id);
+		if(is_null($menuHasCategory)){
+			return response()->json(['message' => 'MenuHasCategory Not Found'], 404);
+		}
+		$menuHasCategory->delete();
+		return response()->json(null, 204);
+	}
 }
