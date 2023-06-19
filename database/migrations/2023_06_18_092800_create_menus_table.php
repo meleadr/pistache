74438a9f824276a55a36cda5975ecfd6cae91f52
meleadr <meledina.adriano@gmail.com>
2023-06-19 18:47:46 +0200

    Ajout de url_img et published pour les menus

--- database/migrations/2023_06_18_092800_create_menus_table.php
+++ database/migrations/2023_06_18_092800_create_menus_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
 			$table->string('title');
 			$table->string('content');
+			$table->string('url_img');
+			$table->boolean('published');
             $table->timestamps();
         });
     }
