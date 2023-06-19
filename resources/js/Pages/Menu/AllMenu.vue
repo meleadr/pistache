9cda9647988c21e6228be271e832bd1caa148625
meleadr <meledina.adriano@gmail.com>
2023-06-19 21:07:44 +0200

    Update AllMenu.vue

--- resources/js/Pages/Menu/AllMenu.vue
+++ resources/js/Pages/Menu/AllMenu.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head } from "@inertiajs/vue3";
+import { Head, Link } from "@inertiajs/vue3";
 import { ref, onMounted, computed } from "vue";
 import axios from "axios";
 import MenuItem from "@/Components/MenuItem.vue";
@@ -46,7 +46,22 @@ onMounted(() => {
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900"></div>
+                    <div class="p-6 text-gray-900">
+                        <div class="flex w-6/12 flex-col">
+                            <Link :href="route('menu.add')">
+                                Ajouter un menu
+                            </Link>
+                        </div>
+                        <div class="flex w-6/12 flex-col">
+                            <input
+                                type="text"
+                                id="search"
+                                name="search"
+                                placeholder="Rechercher un menu"
+                                class="rounded-md border border-gray-300 px-4 py-2 focus:border-indigo-500 focus:ring-indigo-500"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
