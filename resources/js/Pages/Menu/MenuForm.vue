6d6c0641ed60f286341ce61784602688f9de3206
meleadr <meledina.adriano@gmail.com>
2023-06-20 20:21:28 +0200

    Affichage fonctionnel

--- resources/js/Pages/Menu/MenuForm.vue
+++ resources/js/Pages/Menu/MenuForm.vue
@@ -1,9 +1,10 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
-import { ref, toRef, onMounted } from "vue";
+import { ref, toRef, onMounted, computed } from "vue";
 import axios from "axios";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
+import MenuItem from "@/Components/MenuItem.vue";
 
 const props = defineProps({
     id: {
@@ -15,6 +16,7 @@ const props = defineProps({
 const id = toRef(props, "id"); // Making id reactive
 
 let form = ref({
+    id: 0,
     title: "",
     content: "",
     url_img: "",
@@ -22,11 +24,18 @@ let form = ref({
 });
 
 let categories = ref([]);
+let displayCategories = ref([]);
 
 const loadMenuItem = async (id) => {
     const res = await axios.get(`/api/menusWithCategory/${id}`);
-    form.value = res.data;
-    form.value.categories = res.data.categories.map((category) => category.id); // extract the ids
+    const menuData = res.data;
+    // Transform categories into an array of objects with id and name properties
+    menuData.categories = menuData.categories.map((cat) => ({
+        id: cat.id,
+        name: cat.name,
+    }));
+    displayCategories.value = menuData.categories;
+    form.value = menuData;
 };
 
 const loadCategories = async () => {
@@ -48,14 +57,21 @@ let title = ref(
 );
 
 const handleCheckboxChange = (categoryId) => {
-    const index = form.value.categories.indexOf(categoryId);
+    const index = form.value.categories.findIndex(
+        (category) => category.id === categoryId
+    );
 
     if (index > -1) {
         // if the category is already in the array, remove it
         form.value.categories.splice(index, 1);
     } else {
         // if the category is not in the array, add it
-        form.value.categories.push(categoryId);
+        const categoryToAdd = categories.value.find(
+            (category) => category.id === categoryId
+        );
+        if (categoryToAdd) {
+            form.value.categories.push(categoryToAdd);
+        }
     }
 };
 
@@ -78,49 +94,61 @@ const onSubmit = async () => {
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900"></div>
-                </div>
-            </div>
-            <form @submit.prevent="onSubmit" class="mx-auto max-w-xl space-y-3">
-                <input
-                    v-model="form.title"
-                    type="text"
-                    placeholder="Titre"
-                    class="mb-4 mt-4 w-full rounded-lg border px-3 py-2 text-gray-700 focus:outline-none"
-                />
-                <textarea
-                    v-model="form.content"
-                    placeholder="Contenu"
-                    class="mb-2 w-full rounded-lg border px-3 py-2 text-gray-700 focus:outline-none"
-                ></textarea>
-                <div
-                    v-for="category in categories"
-                    :key="category.id"
-                    class="mb-2 flex items-center"
+        <div class="flex flex-col py-12 lg:flex-row">
+            <div class="px-4 sm:mb-10 lg:w-1/2">
+                <form
+                    @submit.prevent="onSubmit"
+                    class="mx-auto max-w-xl space-y-3"
                 >
                     <input
-                        type="checkbox"
-                        :id="category.name"
-                        :value="category.id"
-                        v-model="form.categories"
-                        :checked="form.categories.includes(category.id)"
-                        @change="handleCheckboxChange(category.id)"
-                        class="mr-2"
+                        v-model="form.title"
+                        type="text"
+                        placeholder="Titre"
+                        class="mb-4 mt-4 w-full rounded-lg border px-3 py-2 text-gray-700 focus:outline-none"
                     />
-                    <label :for="category.name" class="text-gray-700">
-                        {{ category.name }}
-                    </label>
-                </div>
+                    <textarea
+                        v-model="form.content"
+                        placeholder="Contenu"
+                        class="mb-2 w-full rounded-lg border px-3 py-2 text-gray-700 focus:outline-none"
+                    ></textarea>
+                    <div
+                        v-for="category in categories"
+                        :key="category.id"
+                        class="mb-2 flex items-center"
+                    >
+                        <input
+                            type="checkbox"
+                            :id="category.name"
+                            :value="category.id"
+                            :checked="
+                                form.categories.some(
+                                    (c) => c.id === category.id
+                                )
+                            "
+                            @change="handleCheckboxChange(category.id)"
+                            class="mr-2"
+                        />
+                        <label :for="category.name" class="text-gray-700">
+                            {{ category.name }}
+                        </label>
+                    </div>
 
-                <div class="flex justify-center">
-                    <PrimaryButton type="submit" class="mx-auto">
-                        {{ props.id != null ? "Modifier" : "Ajouter" }}
-                    </PrimaryButton>
-                </div>
-            </form>
+                    <div class="flex justify-center">
+                        <PrimaryButton type="submit" class="mx-auto">
+                            {{ props.id != null ? "Modifier" : "Ajouter" }}
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+            <div class="px-4 lg:w-1/2">
+                <MenuItem
+                    :id="form.id"
+                    :title="form.title"
+                    :content="form.content"
+                    :url_img="form.url_img"
+                    :categories="displayCategories"
+                />
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
