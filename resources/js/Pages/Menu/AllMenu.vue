1736b200ced2c6b81eeaae4d0fbf60f37a9a2da9
meleadr <meledina.adriano@gmail.com>
2023-06-20 18:46:00 +0200

    bouton ajout

--- resources/js/Pages/Menu/AllMenu.vue
+++ resources/js/Pages/Menu/AllMenu.vue
@@ -1,9 +1,10 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, Link } from "@inertiajs/vue3";
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted } from "vue";
 import axios from "axios";
 import MenuItem from "@/Components/MenuItem.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 // Menu items
 const menuItems = ref([]);
@@ -47,19 +48,23 @@ onMounted(() => {
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <div class="flex w-6/12 flex-col">
-                            <Link :href="route('menu.add')">
-                                Ajouter un menu
-                            </Link>
-                        </div>
-                        <div class="flex w-6/12 flex-col">
-                            <input
-                                type="text"
-                                id="search"
-                                name="search"
-                                placeholder="Rechercher un menu"
-                                class="rounded-md border border-gray-300 px-4 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-                            />
+                        <div
+                            class="flex flex-col justify-between space-y-4 sm:flex-row sm:space-y-0"
+                        >
+                            <div class="w-full sm:w-6/12">
+                                <input
+                                    type="text"
+                                    id="search"
+                                    name="search"
+                                    placeholder="Rechercher un menu"
+                                    class="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500"
+                                />
+                            </div>
+                            <SecondaryButton>
+                                <Link :href="route('menu.add')">
+                                    Ajouter un menu
+                                </Link>
+                            </SecondaryButton>
                         </div>
                     </div>
                 </div>
@@ -67,7 +72,9 @@ onMounted(() => {
         </div>
 
         <!-- Menu Items -->
-        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div
+            class="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 lg:px-8"
+        >
             <div v-for="item in menuItems" :key="item.id">
                 <MenuItem
                     :id="item.id"
