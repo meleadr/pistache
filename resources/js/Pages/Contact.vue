18c5c5a6d145da6cee8e05589394d06c5b273084
meleadr <meledina.adriano@gmail.com>
2023-06-26 18:54:14 +0200

    Taille de la map

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -14,13 +14,13 @@ const contactInformation = {
 <template>
     <Head title="Contactez-nous" />
 
-    <WebsiteLayout class="mr-10 ml-10 mb-10">
+    <WebsiteLayout class="mb-10 ml-10 mr-10">
         <template #header>
-            <h2 class="text-4xl text-center mb-10">Nous Contacter</h2>
+            <h2 class="mb-10 text-center text-4xl">Nous Contacter</h2>
         </template>
         <!-- Contact Information Section -->
         <section class="contact-info py-20">
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 text-center">
+            <div class="grid grid-cols-1 gap-6 text-center sm:grid-cols-2">
                 <div>
                     <p class="text-xl">Adresse</p>
                     <p class="text-xl">{{ contactInformation.address }}</p>
@@ -38,9 +38,9 @@ const contactInformation = {
 
         <!-- Google Map Section -->
         <section class="map w-full">
-            <div class="aspect-w-16 aspect-h-9 w-full">
+            <div class="h-80">
                 <iframe
-                    class="w-full h-full"
+                    class="h-full w-full"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.761666666667!2d2.336761315674781!3d48.86272597928894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x4b9b0c1b0c0b0b0!2sPistache!5e0!3m2!1sfr!2sfr!4v1623349129129!5m2!1sfr!2sfr"
                     style="border: 0"
                     allowfullscreen=""
