65f19aeba731a8559e01ba461d4415b2baf6b709
meleadr <meledina.adriano@gmail.com>
2023-06-26 18:50:16 +0200

    Nouvelles photos

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -21,31 +21,31 @@ class MenuSeeder extends Seeder
 		\App\Models\Menu::factory()->create([
 			'title' => 'Coq au Vin',
 			'content' => 'Poulet, vin rouge, champignons...',
-			'url_img' => '/img/menu/1.webp',
+			'url_img' => '/img/menu/2.jpg',
 			'published' => 1,
 		]);
 		\App\Models\Menu::factory()->create([
 			'title' => 'Moules Marinières',
 			'content' => 'Moules, vin blanc, oignons...',
-			'url_img' => '/img/menu/1.webp',
+			'url_img' => '/img/menu/3.jpg',
 			'published' => 1,
 		]);
 		\App\Models\Menu::factory()->create([
 			'title' => 'Tarte aux pommes',
 			'content' => 'Pommes, pâte, sucre...',
-			'url_img' => '/img/menu/1.webp',
+			'url_img' => '/img/menu/4.jpg',
 			'published' => 1,
 		]);
 		\App\Models\Menu::factory()->create([
-			'title' => 'Sol au sel',
+			'title' => 'Sole au sel',
 			'content' => 'Sole, sel, citron...',
-			'url_img' => '/img/menu/1.webp',
+			'url_img' => '/img/menu/5.jpg',
 			'published' => 1,
 		]);
 		\App\Models\Menu::factory()->create([
 			'title' => 'Salade de la mer',
 			'content' => 'Salade, tomates, olives, œuf, crevettes, saumon...',
-			'url_img' => '/img/menu/1.webp',
+			'url_img' => '/img/menu/6.jpg',
 			'published' => 1,
 		]);
     }
